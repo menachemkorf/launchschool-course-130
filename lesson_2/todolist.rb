56a91c3a108bd7e0cd570a096d416441617179a6
Menachem Korf <menachemkorf@gmail.com>
2016-07-31 22:23:05 +1000

    finish lesson 2 - unit testing

---
+++ lesson_2/todolist.rb
@@ -0,0 +1,160 @@
+class Todo
+  DONE_MARKER = "X"
+  UNDONE_MARKER = " "
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description="")
+    @title = title
+    @discription = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def <<(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.instance_of? Todo
+    @todos << todo
+  end
+  alias_method :add, :<<
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def remove_at(index)
+    @todos.delete(item_at(index))
+  end
+
+  # def to_s
+  #   puts "---- #{title} ----"
+  #   @todos.each { |todo| puts todo }
+  # end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos
+  end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+
+  def select
+    list = TodoList.new(title)
+    each do |todo|
+      list << todo if yield(todo)
+    end
+    list
+  end
+
+  def find_by_title(str)
+    select { |todo| todo.title == str }.first
+  end
+
+  def all_done
+    select(&:done?)
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each(&:done!)
+  end
+
+  def mark_all_undone
+    each(&:undone!)
+  end
+
+  def done?
+    @todos.all?(&:done?)
+  end
+
+  def done!
+    each(&:done!)
+  end
+end
+
+# todo1 = Todo.new("Buy milk")
+# todo2 = Todo.new("Clean room")
+# todo3 = Todo.new("Go to gym")
+# list = TodoList.new("Today's Todos")
+
+# list.add(todo1)                 # adds todo1 to end of list, returns list
+# list.add(todo2)                 # adds todo2 to end of list, returns list
+# list.add(todo3)                 # adds todo3 to end of list, returns list
+
+# todo1.done!
+
+# # list.mark_all_done
+# # list.mark_all_undone
+# list.mark_done("Buy mi")
+
+# puts list
+
+
